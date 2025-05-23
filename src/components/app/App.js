be3228343b9159b54f245881cdc8e99dc1d31631
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2025-05-23 18:08:49 +0300

    optimization React.lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,13 @@
+import {lazy, Suspense} from 'react';
+
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import { MainPage, ComicsPage, Page404, SingleComicPage} from "../../pages";
 
 import AppHeader from "../appHeader/AppHeader";
+
+const Page404 = lazy(() => import('../../pages/Page404'));
+const MainPage = lazy(() => import('../../pages/MainPage'));
+const ComicsPage = lazy(() => import('../../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../../pages/SingleComicPage'));
 
 const App = () => {
     return (
@@ -9,20 +15,22 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Switch>
-                        <Route exact path='/'>
-                            <MainPage/>
-                        </Route>
-                        <Route exact path='/comics'>
-                            <ComicsPage/>
-                        </Route>
-                        <Route path='/comics/:comicId'>
-                            <SingleComicPage/>
-                        </Route>
-                        <Route path='*'>
-                            <Page404/>
-                        </Route>
-                    </Switch>
+                    <Suspense>
+                        <Switch>
+                            <Route exact path='/'>
+                                <MainPage/>
+                            </Route>
+                            <Route exact path='/comics'>
+                                <ComicsPage/>
+                            </Route>
+                            <Route path='/comics/:comicId'>
+                                <SingleComicPage/>
+                            </Route>
+                            <Route path='*'>
+                                <Page404/>
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </main>
             </div>
         </Router>

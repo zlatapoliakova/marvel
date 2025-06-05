b2306258e02d898413defa288e126b4cc3d10044
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2025-06-05 15:47:10 +0300

    homework/ add process in searchForm

--- src/components/searchForm/SearchForm.js
+++ src/components/searchForm/SearchForm.js
@@ -8,10 +8,29 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './searchForm.scss';
 
+const setContent = (process, Component, data) => {
+    switch(process) {
+        case 'waiting':
+            return;
+        case 'loading':
+            return;
+        case 'confirmed':
+            return data.length > 0 ? 
+                <Component data={data} />: 
+                <div className='form_error'>
+                    The character was not found. Check the name and try again
+                </div>; 
+        case 'error':
+            return <div className='form_gif-error'><ErrorMessage/></div>;
+        default:
+            throw new Error('Unexpected state process');
+    }
+}
+
 const SearchForm = () => {
     
-    const {loading, error, getCharactersByName, clearError} = useMarvelServices();
-    const [chars, setChars] = useState(null);
+    const {getCharactersByName, clearError, process, setProcess} = useMarvelServices();
+    const [chars, setChars] = useState([]);
 
     const onCharLoaded = (char) => {
         setChars(char);
@@ -21,24 +40,9 @@ const SearchForm = () => {
         clearError();
 
         getCharactersByName(name)
-            .then(onCharLoaded);
+            .then(onCharLoaded)
+            .then(() => setProcess('confirmed'));
     }
-
-    const errorMsg = error ? <div className='form_gif-error'><ErrorMessage/></div> : null;
-    const result = !chars ? null : chars.length > 0 ?  
-                    <div className='form_success'>
-                        <div className="form_success_text">There is! Visit {chars[0].name} page?</div>
-                        <Link to={`/characters/${chars[0].id}`}>
-                            <button className="button button__secondary" >
-                                <div className="inner">
-                                    To page
-                                </div>
-                            </button> 
-                        </Link>
-                    </div> :
-                    <div className='form_error'>
-                        The character was not found. Check the name and try again
-                    </div>;
 
     return (
         <div className="form">
@@ -47,32 +51,49 @@ const SearchForm = () => {
                 validationSchema={Yup.object({
                     name: Yup.string().required("This field is required")
                 })}
-                onSubmit={({name}) => {
-                    updateChar(name);
+                onSubmit={(values, {setSubmitting}) => {
+                    updateChar(values.name);
+                    setSubmitting(false);
                 }}>
-                <Form>
-                    <label htmlFor="name" className='form_title'>Or find a character by name:</label>
-                    <div className="form_content">
-                        <Field 
-                            id="name" 
-                            name="name" 
-                            type="text"
-                            placeholder="Enter name"
-                            className="form_field" />
-                        <button 
-                            className="button button__main"
-                            type="submit"
-                            disabled={loading}>
-                            <div className="inner">
-                                FIND
-                            </div>
-                        </button>
-                    </div>
-                    <FormikErrorMessage className="form_error" name="name" component="div"/>
-                </Form>   
+                {({isSubmitting}) => (
+                    <Form>
+                        <label htmlFor="name" className='form_title'>Or find a character by name:</label>
+                        <div className="form_content">
+                            <Field 
+                                id="name" 
+                                name="name" 
+                                type="text"
+                                placeholder="Enter name"
+                                className="form_field" />
+                            <button 
+                                className="button button__main"
+                                type="submit"
+                                disabled={isSubmitting}>
+                                <div className="inner">
+                                    FIND
+                                </div>
+                            </button>
+                        </div>
+                        <FormikErrorMessage className="form_error" name="name" component="div"/>
+                    </Form>
+                )}   
             </Formik>
-            {errorMsg}
-            {result} 
+            {setContent(process, SuccessMessage, chars)}
+        </div>
+    );
+}
+
+const SuccessMessage = ({data}) => {
+    return(
+        <div className='form_success'>
+            <div className="form_success_text">There is! Visit {data[0].name} page?</div>
+            <Link to={`/characters/${data[0].id}`}>
+                <button className="button button__secondary" >
+                    <div className="inner">
+                        To page
+                    </div>
+                </button> 
+            </Link>
         </div>
     );
 }
